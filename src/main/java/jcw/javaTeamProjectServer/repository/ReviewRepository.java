477274e5a56e400e9f705dba3815a47e5a14532e
woopaca <jcw001031@gmail.com>
2022-11-30 16:03:18 +0900

    parameter 오류 확인

--- src/main/java/jcw/javaTeamProjectServer/repository/ReviewRepository.java
+++ src/main/java/jcw/javaTeamProjectServer/repository/ReviewRepository.java
@@ -3,6 +3,7 @@ package jcw.javaTeamProjectServer.repository;
 import jcw.javaTeamProjectServer.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +14,5 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByItemKey(Long itemKey);
 
     @Query(value = "SELECT AVG(r.reviewStar) FROM Review r WHERE r.itemKey = :itemKey GROUP BY r.itemKey")
-    double avgRating(Long itemKey);
+    double avgRating(@Param("itemKey") Long itemKey);
 }
